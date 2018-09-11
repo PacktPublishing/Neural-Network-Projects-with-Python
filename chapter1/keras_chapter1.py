46b9b1568e3309fcb63c8c9e7f3935cf17fc2e7d
James <jamesloyys@gmail.com>
2018-09-11 21:56:41 +0800

    Create keras_chapter1.py

---
+++ chapter1/keras_chapter1.py
@@ -0,0 +1,27 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import optimizers
+import numpy as np
+
+model = Sequential()
+
+# Layer 1
+model.add(Dense(units=4, activation='sigmoid', input_dim=3))
+# Output Layer
+model.add(Dense(units=1, activation='sigmoid'))
+
+print(model.summary())
+print('')
+
+sgd = optimizers.SGD(lr=1)
+model.compile(loss='mean_squared_error', optimizer=sgd)
+
+X = np.array([[0,0,1],
+              [0,1,1],
+              [1,0,1],
+              [1,1,1]])
+y = np.array([[0],[1],[1],[0]])
+
+model.fit(X, y, epochs=1500, verbose=False)
+
+print(model.predict(X))
