c808c65c315957725aa9454d1199a30fcf18f2ac
James <jamesloyys@gmail.com>
2018-10-29 18:56:38 +0800

    Add files via upload

---
+++ chapter2/main.py
@@ -0,0 +1,53 @@
+from utils import preprocess
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve
+from keras.models import Sequential
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('https://raw.githubusercontent.com/PacktPublishing/Neural-Network-Projects-with-Python/master/chapter2/diabetes.csv')
+
+# Perform preprocessing and feature engineering
+df = preprocess(df)
+
+# Split the data into a training and testing set
+X = df.loc[:, df.columns != 'Outcome']
+y = df.loc[:, 'Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Build neural network in Keras
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=8))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=200, verbose=False)
+
+# Results - Accuracy
+scores = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: %.2f%%\n" % (scores[1]*100))
+scores = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy: %.2f%%\n" % (scores[1]*100))
+
+# Results - Confusion Matrix
+y_test_pred = model.predict_classes(X_test)
+c_matrix = confusion_matrix(y_test, y_test_pred)
+ax = sns.heatmap(c_matrix, annot=True, xticklabels=['No Diabetes', 'Diabetes'], yticklabels=['No Diabetes', 'Diabetes'], cbar=False, cmap='Blues')
+ax.set_xlabel("Prediction")
+ax.set_ylabel("Actual")
+plt.show()
+plt.clf()
+
+# Results - ROC Curve
+y_test_pred_probs = model.predict(X_test)
+FPR, TPR, _ = roc_curve(y_test, y_test_pred_probs)
+plt.plot(FPR, TPR)
+plt.plot([0,1],[0,1],'--', color='black') #diagonal line
+plt.title('ROC Curve')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.show()
+plt.clf()
---
+++ chapter2/utils.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
+
+def preprocess(df):
+    # Replace 0 values with the mean of the existing values
+    df['Glucose'] = df['Glucose'].replace(0, np.nan)
+    df['BloodPressure'] = df['BloodPressure'].replace(0, np.nan)
+    df['SkinThickness'] = df['SkinThickness'].replace(0, np.nan)
+    df['Insulin'] = df['Insulin'].replace(0, np.nan)
+    df['BMI'] = df['BMI'].replace(0, np.nan)
+    df['Glucose'] = df['Glucose'].fillna(df['Glucose'].mean())
+    df['BloodPressure'] = df['BloodPressure'].fillna(df['BloodPressure'].mean())
+    df['SkinThickness'] = df['SkinThickness'].fillna(df['SkinThickness'].mean())
+    df['Insulin'] = df['Insulin'].fillna(df['Insulin'].mean())
+    df['BMI'] = df['BMI'].fillna(df['BMI'].mean())
+
+    # Standardization
+    df_scaled = preprocessing.scale(df)
+    df_scaled = pd.DataFrame(df_scaled, columns=df.columns)
+    df_scaled['Outcome'] = df['Outcome']
+    df = df_scaled
+
+    return df
