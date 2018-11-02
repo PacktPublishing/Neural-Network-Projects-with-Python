1ff881e490d8c14e373b4d036ece57f7ea6dfc9b
James <jamesloyys@gmail.com>
2018-11-02 11:13:30 +0800

    Add files via upload

---
+++ chapter3/visualize.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('NYC_taxi_truncated.csv', parse_dates=['pickup_datetime'])
+
+print(df.head())
+
+## Visualizing Geolocation Data #############################################
+
+# range of longitude for NYC
+nyc_min_longitude = -74.05
+nyc_max_longitude = -73.75
+
+# range of latitude for NYC
+nyc_min_latitude = 40.63
+nyc_max_latitude = 40.85
+
+df2 = df.copy(deep=True)
+for long in ['pickup_longitude', 'dropoff_longitude']:
+    df2 = df2[(df2[long] > nyc_min_longitude) & (df2[long] < nyc_max_longitude)]
+
+for lat in ['pickup_latitude', 'dropoff_latitude']:
+    df2 = df2[(df2[lat] > nyc_min_latitude) & (df2[lat] < nyc_max_latitude)]
+
+landmarks = {'JFK Airport': (-73.78,40.643),
+             'Laguardia Airport': (-73.87, 40.77),
+             'Midtown': (-73.98, 40.76),
+             'Lower Manhattan': (-74.00, 40.72),
+             'Upper Manhattan': (-73.94, 40.82)}
+
+
+def plot_lat_long(df, landmarks, points='Pickup'):
+    plt.figure(figsize = (12,12)) # set figure size
+    if points == 'pickup':
+        plt.plot(list(df.pickup_longitude), list(df.pickup_latitude), '.', markersize=1)
+    else:
+        plt.plot(list(df.dropoff_longitude), list(df.dropoff_latitude), '.', markersize=1)
+
+    for landmark in landmarks:
+        plt.plot(landmarks[landmark][0], landmarks[landmark][1], '*', markersize=15, alpha=1, color='r') # plot landmark location on map
+        plt.annotate(landmark, (landmarks[landmark][0]+0.005, landmarks[landmark][1]+0.005), color='r', backgroundcolor='w') # add 0.005 offset on landmark name for aesthetics purposes
+  
+    plt.title("{} Locations in NYC Illustrated".format(points))
+    plt.grid(None)
+    plt.xlabel("Latitude")
+    plt.ylabel("Longitude")
+    plt.show()
+
+
+plot_lat_long(df2, landmarks, points='Pickup')
+
+plot_lat_long(df2, landmarks, points='Drop Off')
+
+
+
+## Ridership by Day and Hour #############################################
+
+df['year'] = df['pickup_datetime'].dt.year
+df['month'] = df['pickup_datetime'].dt.month
+df['day'] = df['pickup_datetime'].dt.day
+df['day_of_week'] = df['pickup_datetime'].dt.dayofweek
+df['hour'] = df['pickup_datetime'].dt.hour
+
+df['day_of_week'].plot.hist(bins=np.arange(8)-0.5, ec='black', ylim=(60000,75000))
+plt.xlabel('Day of Week (0=Monday, 6=Sunday)')
+plt.title('Day of Week Histogram')
+plt.show()
+
+df['hour'].plot.hist(bins=24, ec='black')
+plt.title('Pickup Hour Histogram')
+plt.xlabel('Hour')
+plt.show()
+
+
+
+
