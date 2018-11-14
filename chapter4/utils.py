73b66319f8d037de3683d61fa49a273981d5361a
James <jamesloyys@gmail.com>
2018-11-14 10:43:55 +0800

    Add files via upload

---
+++ chapter4/utils.py
@@ -0,0 +1,72 @@
+import os
+import random
+import shutil
+import piexif
+
+def train_test_split(src_folder, train_size = 0.8):
+	# Make sure we remove any existing folders and start from a clean slate
+	shutil.rmtree(src_folder+'Train/Cat/', ignore_errors=True)
+	shutil.rmtree(src_folder+'Train/Dog/', ignore_errors=True)
+	shutil.rmtree(src_folder+'Test/Cat/', ignore_errors=True)
+	shutil.rmtree(src_folder+'Test/Dog/', ignore_errors=True)
+
+	# Now, create new empty train and test folders
+	os.makedirs(src_folder+'Train/Cat/')
+	os.makedirs(src_folder+'Train/Dog/')
+	os.makedirs(src_folder+'Test/Cat/')
+	os.makedirs(src_folder+'Test/Dog/')
+
+	# Get the number of cats and dogs images
+	_, _, cat_images = next(os.walk(src_folder+'Cat/'))
+	files_to_be_removed = ['Thumbs.db', '666.jpg', '835.jpg']
+	for file in files_to_be_removed:
+		cat_images.remove(file)
+	num_cat_images = len(cat_images)
+	num_cat_images_train = int(train_size * num_cat_images)
+	num_cat_images_test = num_cat_images - num_cat_images_train
+
+	_, _, dog_images = next(os.walk(src_folder+'Dog/'))
+	files_to_be_removed = ['Thumbs.db', '11702.jpg']
+	for file in files_to_be_removed:
+		dog_images.remove(file)
+	num_dog_images = len(dog_images)
+	num_dog_images_train = int(train_size * num_dog_images)
+	num_dog_images_test = num_dog_images - num_dog_images_train
+
+	# Randomly assign images to train and test
+	cat_train_images = random.sample(cat_images, num_cat_images_train)
+	for img in cat_train_images:
+		shutil.copy(src=src_folder+'Cat/'+img, dst=src_folder+'Train/Cat/')
+	cat_test_images  = [img for img in cat_images if img not in cat_train_images]
+	for img in cat_test_images:
+		shutil.copy(src=src_folder+'Cat/'+img, dst=src_folder+'Test/Cat/')
+
+	dog_train_images = random.sample(dog_images, num_dog_images_train)
+	for img in dog_train_images:
+		shutil.copy(src=src_folder+'Dog/'+img, dst=src_folder+'Train/Dog/')
+	dog_test_images  = [img for img in dog_images if img not in dog_train_images]
+	for img in dog_test_images:
+		shutil.copy(src=src_folder+'Dog/'+img, dst=src_folder+'Test/Dog/')
+
+# helper function to remove corrupt exif data from Microsoft's dataset
+def remove_exif_data(src_folder):
+	_, _, cat_images = next(os.walk(src_folder+'Cat/'))
+	for img in cat_images:
+		try:
+			piexif.remove(src_folder+'Cat/'+img)
+		except:
+			pass
+
+	_, _, dog_images = next(os.walk(src_folder+'Dog/'))
+	for img in dog_images:
+		try:
+			piexif.remove(src_folder+'Dog/'+img)
+		except:
+			pass
+
+
+
+src_folder = 'Dataset/PetImages/'
+train_test_split(src_folder)
+remove_exif_data(src_folder+'Train/')
+remove_exif_data(src_folder+'Test/')
