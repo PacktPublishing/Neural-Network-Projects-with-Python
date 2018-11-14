dfeb559636ad7934bae775b65c12f2966d33e795
James <jamesloyys@gmail.com>
2018-11-14 17:08:50 +0800

    Add files via upload

---
+++ chapter4/visualize_dataset.py
@@ -0,0 +1,35 @@
+from matplotlib import pyplot as plt
+import os
+import random
+
+# Get list of file names
+_, _, cat_images = next(os.walk('Dataset/PetImages/Cat'))
+
+# Prepare a 3x3 plot (total of 9 images)
+fig, ax = plt.subplots(3,3, figsize=(20,10))
+
+# Randomly select and plot an image
+for idx, img in enumerate(random.sample(cat_images, 9)):
+    img_read = plt.imread('Dataset/PetImages/Cat/'+img)
+    ax[int(idx/3), idx%3].imshow(img_read)
+    ax[int(idx/3), idx%3].axis('off')
+    ax[int(idx/3), idx%3].set_title('Cat/'+img)
+
+plt.show()
+
+
+# Get list of file names
+_, _, dog_images = next(os.walk('Dataset/PetImages/Dog'))
+
+# Prepare a 3x3 plot (total of 9 images)
+fig, ax = plt.subplots(3,3, figsize=(20,10))
+
+# Randomly select and plot an image
+for idx, img in enumerate(random.sample(dog_images, 9)):
+    img_read = plt.imread('Dataset/PetImages/Dog/'+img)
+    ax[int(idx/3), idx%3].imshow(img_read)
+    ax[int(idx/3), idx%3].axis('off')
+    ax[int(idx/3), idx%3].set_title('Dog/'+img)
+
+plt.show()
+
