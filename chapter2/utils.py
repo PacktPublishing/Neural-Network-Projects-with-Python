86411305eba7298bfc4d83b76a8de5f986a33de3
James <jamesloyys@gmail.com>
2019-02-26 01:46:48 +0800

    Add files via upload

--- chapter2/utils.py
+++ chapter2/utils.py
@@ -4,6 +4,14 @@ from sklearn.preprocessing import StandardScaler
 from sklearn import preprocessing
 
 def preprocess(df):
+    print('----------------------------------------------')
+    print("Before preprocessing")
+    print("Number of rows with 0 values for each variable")
+    for col in df.columns:
+        missing_rows = df.loc[df[col]==0].shape[0]
+        print(col + ": " + str(missing_rows))
+    print('----------------------------------------------')
+
     # Replace 0 values with the mean of the existing values
     df['Glucose'] = df['Glucose'].replace(0, np.nan)
     df['BloodPressure'] = df['BloodPressure'].replace(0, np.nan)
@@ -21,5 +29,13 @@ def preprocess(df):
     df_scaled = pd.DataFrame(df_scaled, columns=df.columns)
     df_scaled['Outcome'] = df['Outcome']
     df = df_scaled
+    
+    print('----------------------------------------------')
+    print("After preprocessing")
+    print("Number of rows with 0 values for each variable")
+    for col in df.columns:
+        missing_rows = df.loc[df[col]==0].shape[0]
+        print(col + ": " + str(missing_rows))
+    print('----------------------------------------------')
 
     return df
